baa94be94c0cc9c6b6e9f42c4067135aa776aa00
Fatoumatasona <sonasylla26@gmail.com>
2026-04-23 11:56:19 +0200

    Création et implementation du projet RelevésMeteo

---
+++ RelevésMétéo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace RelevésMétéo
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         AfficherListe();
+         Console.WriteLine();
+      }
+      static void AfficherListe()
+      {
+         string[] lignes = File.ReadAllLines("MeteoParis.csv");
+         float sommeTemp = 0f;
+         for (int i = 1; i < lignes.Length; i++)
+         {
+            // Simplifie le format des heures d'ensoleillement
+            string ligne = lignes[i].Replace("h ", "h").Replace("min", "");
+
+            // Recupère les infos de la ligne dans un tableau
+            string[] infos = ligne.Split(';');
+
+            Console.WriteLine($"{infos[0]}/{infos[1]} : [{infos[2]} ; {infos[3]}]°C\t{infos[6]} de soleil\t {infos[7]} mm de pluie");
+
+            // Ajoute la température moyenne au cumul
+            if(float.TryParse(infos[4], out float temp))
+               sommeTemp += temp;
+         }
+
+         Console.WriteLine();
+         Console.WriteLine($"T° moyenne globale : {sommeTemp / (lignes.Length - 1)}");
+      }
+   }
+}
